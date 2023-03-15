2322cb80575d7e3ab4a3946f27bed1f4d7427fb4
NIK-HCJ <126838573+NIK-HCJ@users.noreply.github.com>
2023-03-14 21:35:03 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+/**FUNCIÓN DE LOS BOTONES*/
+
+function B1() {
+    var x = document.getElementById("1E");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function B2() {
+    var x = document.getElementById("1D");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
